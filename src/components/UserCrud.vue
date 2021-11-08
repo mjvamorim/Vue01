a57a5627d5a59d04e71d14150bc285102514c8fa
Mauricio Amorim <mjvamorim@gmail.com>
2021-11-08 18:15:35 -0300

    Formulario

--- src/components/UserCrud.vue
+++ src/components/UserCrud.vue
@@ -17,6 +17,69 @@
               class="mx-4"
             ></v-text-field>
           </template>
+
+          ##Formulario
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>My CRUD</v-toolbar-title>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="700px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Novo Item
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">UserCrud</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.id"
+                            label="Id"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="8">
+                          <v-text-field
+                            v-model="editedItem.nome"
+                            label="Nome"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.pais"
+                            label="Localização"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="close">
+                      Cancel
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="save">
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          ##Fim Form
         </v-data-table>
       </v-col>
     </v-row>
@@ -24,22 +87,48 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "UserCrud",
 
   data: () => ({
     search: "",
+    dialog: false,
     headers: [
       { text: "Id", value: "id" },
       { text: "Nome", value: "nome" },
       { text: "Localização", value: "pais" },
       { text: "Actions", value: "actions", sortable: false },
     ],
-    usuarios: [
-      { id: 1, nome: "Joao", pais: "Brasil", tipo_id: "1" },
-      { id: 2, nome: "Pedro", pais: "Portugal", tipo_id: "1" },
-      { id: 3, nome: "Maria", pais: "Brasil", tipo_id: "1" },
-    ],
+    usuarios: [],
+    editedItem: { id: 0, nome: "", pais: "Brasil" },
   }),
+  methods: {
+    inicializa() {
+      axios
+        .get("http://localhost:3000/usuarios")
+        .then((response) => {
+          this.usuarios = response.data;
+        })
+        .catch((error) => console.log(error));
+    },
+    save() {
+      axios
+        .post("http://localhost:3000/usuarios", this.editedItem)
+        .then((response) => {
+          console.log(response);
+          this.dialog = false;
+          this.inicializa();
+          this.editedItem = { id: 0, nome: "", pais: "Brasil" };
+        })
+        .catch((error) => console.log(error));
+    },
+    close() {
+      this.dialog = false;
+    },
+  },
+  created() {
+    this.inicializa();
+  },
 };
 </script>
